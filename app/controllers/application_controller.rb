a1f66894712e195edf4918856f231806a49032cb
Cleveland Williams <clevelandwilliams3579@gmail.com>
2017-01-18 08:58:36 -0500

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    #User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+    redirect_to root_path
+    end
   end
   helper_method :current_user
 end
